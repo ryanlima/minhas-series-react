d0fba0573d435f0fbd76802cc0eca135617a5f2a
Ryan Lima <ryanlima08021997@gmail.com>
2019-06-21 19:15:51 -0300

    Minhas Series React Dev Pleno

---
+++ src/Api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:3001/'
+})
+
+export const loadGenres = () => api.get('genres')
+export const saveSeries = (newSeries) => api.post('series', newSeries)
+export const loadSeriesByGenre = (genre) => api.get('series?genre='+genre)
+const apis = {
+    loadGenres: loadGenres,
+    saveSeries: saveSeries,
+    loadSeriesByGenre: loadSeriesByGenre
+}
+export default apis
--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './Home'
+import Series from './Series'
+import NewSeries from './NewSeries'
+
+// Functional-stateless component
+const About = () => <section className="intro-section"><h1>Sobre</h1></section>
+
+
+class App extends Component{
+
+  render(){
+    return (
+      <Router>
+        <div>
+          <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div className="container">
+              <div className="navbar-header page-scroll">
+                <a className="navbar-brand page-scroll" href="#page-top">
+                    <img src="images/logo.png" height="30" alt="Logo Netflix"/>
+                </a>
+              </div>
+    
+              <div className="collapse navbar-collapse navbar-ex1-collapse">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <Link to='/'>Home</Link>
+                  </li>
+                  <li>
+                    <Link to='/new'>Nova Série</Link>
+                  </li>
+                  <li>
+                    <Link to='/about'>Sobre</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <Route exact path='/' component={Home}/>
+          <Route path='/series/:genre' component={Series}/>
+          <Route exact path='/new' component={NewSeries}/>
+          <Route exact path='/about' component={About}/>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+
+export default App
